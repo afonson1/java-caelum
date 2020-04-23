89c0be396b6e0cb32128012718832abe8b1e037f
Afonso Serafim Jacinto <aphonson1@gmail.com>
2020-04-23 19:50:16 -0300

    Create TestaJar.java

---
+++ src/br/com/caelum/contas/main/TestaJar.java
@@ -0,0 +1,13 @@
+package br.com.caelum.contas.main;
+
+//import br.com.caelum.javafx.api.main.OlaMundo;
+import br.com.caelum.javafx.api.main.TelaDeContas;
+
+public class TestaJar {
+
+	public static void main(String[] args) {
+		//OlaMundo.main(args);
+		TelaDeContas.main(args);
+	}
+
+}
