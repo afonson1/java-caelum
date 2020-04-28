1b5993eac0a79ae2f60acdb482ec3b6a74a3c748
Afonso Serafim Jacinto <aphonson1@gmail.com>
2020-04-28 16:22:06 -0300

    Update Funcionario.java

--- src/br/com/caelum/contas/modelo/Funcionario.java
+++ src/br/com/caelum/contas/modelo/Funcionario.java
@@ -1,10 +1,38 @@
 package br.com.caelum.contas.modelo;
 
 public class Funcionario {
-	private String nome;
-	private String cpf;
-	private double salario;
+	protected String nome;
+	protected String cpf;
+	protected double salario;
 	
-	//métodos devem vir aqui
+	/*public Funcionario(String nome, String cpf, double salario){
+		this.nome = nome;
+		this.cpf = cpf;
+		this.salario = salario;
+	}*/
+	
+	public String getNome(){
+		return nome;
+	}
+	
+	public void setNome(String nome){
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public double getSalario() {
+		return salario;
+	}
+
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
 
 }
