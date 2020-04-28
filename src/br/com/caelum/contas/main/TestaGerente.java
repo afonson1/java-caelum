4238f3452aa2b00914cc4f231de22f7ccbb20111
Afonso Serafim Jacinto <aphonson1@gmail.com>
2020-04-28 16:22:03 -0300

    Create TestaGerente.java

---
+++ src/br/com/caelum/contas/main/TestaGerente.java
@@ -0,0 +1,15 @@
+package br.com.caelum.contas.main;
+
+import br.com.caelum.contas.modelo.Gerente;
+
+public class TestaGerente {
+
+	public static void main(String[] args) {
+		Gerente gerente = new Gerente();
+		
+		gerente.setNome("Afonso Serafim");
+		
+		gerente.setSenha(12345);
+	}
+
+}
