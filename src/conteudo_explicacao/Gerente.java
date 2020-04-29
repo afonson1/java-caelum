050a2ad95872c47a1f4cfdbc8d5b096b80c2d32b
Afonso Serafim Jacinto <aphonson1@gmail.com>
2020-04-29 20:30:12 -0300

    Revert "Delete Gerente.java"
    This reverts commit 8a5abf8139c8c6af9840f5f080ef88a14ef15a04.

---
+++ src/conteudo_explicacao/Gerente.java
@@ -0,0 +1,39 @@
+package conteudo_explicacao;
+
+
+public class Gerente extends Funcionario {
+	private int senha;
+	private int numeroDeFuncionariosGerenciados;
+
+	//Getters e Setters
+	public void setSenha(int senha) {
+		this.senha = senha;
+	}
+	
+	public int getNumeroDeFuncionariosGerenciados() {
+		return numeroDeFuncionariosGerenciados;
+	}
+
+	public void setNumeroDeFuncionariosGerenciados(
+			int numeroDeFuncionariosGerenciados) {
+		this.numeroDeFuncionariosGerenciados = numeroDeFuncionariosGerenciados;
+	}
+	
+	//Métodos
+	public boolean autentica(int senha){
+		if (this.senha == senha){
+			System.out.println("Acesso Permitido!");
+			return true;
+		}else{
+			System.out.println("Acesso Negado!");
+			return false;
+		}
+	}
+
+	@Override
+	public double getBonificacao(){
+		//O super foi utilizado para chamar o método 'getBonificacao' de uma super classe de Gerente.
+		//return super.getBonificacao();
+		return salario * 0.15;
+	}
+}
