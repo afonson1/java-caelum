eefb8e517de9504dd7ecd97de0ba9e2ed2ad3064
Afonso Serafim Jacinto <aphonson1@gmail.com>
2020-04-30 15:30:15 -0300

    Update TestaBonificacoes.java

--- src/conteudo_explicacao/TestaBonificacoes.java
+++ src/conteudo_explicacao/TestaBonificacoes.java
@@ -4,17 +4,7 @@ package conteudo_explicacao;
 public class TestaBonificacoes {
 
 	public static void main(String[] args) {
-		ControleDeBonificacoes controle = new ControleDeBonificacoes();
 		
-		Gerente funcionario1 = new Gerente();
-		funcionario1.setSalario(5000.0);
-		controle.registra(funcionario1);
-		
-		Funcionario funcionario2 = new Funcionario();
-		funcionario2.setSalario(1000.0);
-		controle.registra(funcionario2);
-		
-		System.out.println(controle.getTotalDeBonificacoes());
 	}
 
 }
