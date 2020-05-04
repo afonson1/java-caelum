0e6b5bf050769745e9928a329a8ba1f3a0d2068f
Afonso Serafim Jacinto <aphonson1@gmail.com>
2020-05-04 16:00:36 -0300

    Update TesteErro.java

--- src/conteudo_explicacao/TesteErro.java
+++ src/conteudo_explicacao/TesteErro.java
@@ -6,7 +6,11 @@ public class TesteErro {
 
 	public static void main(String[] args) {
 		System.out.println("Inicio do main");
-		metodo1();
+		try {
+			metodo1();
+		} catch (NullPointerException e){
+			System.out.println("erro: " + e);
+		}
 		System.out.println("Fim do main");
 	}
 	
@@ -20,14 +24,10 @@ public class TesteErro {
 		System.out.println("Inicio do metodo2");
 		ContaCorrente cc = new ContaCorrente();
 		for (int i = 0; i <=15; i++){
-			try {
-				cc.deposita(i + 1000);
-				System.out.println(cc.getSaldo());
-				if (i == 5){
-					cc = null;
-				}
-			} catch (NullPointerException e){
-				System.out.println("erro: " + e);
+			cc.deposita(i + 1000);
+			System.out.println(cc.getSaldo());
+			if (i == 5){
+				cc = null;
 			}
 		}
 		System.out.println("Fim do metodo2");
