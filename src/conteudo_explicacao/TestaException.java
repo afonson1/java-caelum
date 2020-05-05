9d5e8d689392d23cd7c0c413ebbc7c1b69419a2a
Afonso Serafim Jacinto <aphonson1@gmail.com>
2020-05-05 15:20:28 -0300

    Create TestaException.java

---
+++ src/conteudo_explicacao/TestaException.java
@@ -0,0 +1,11 @@
+package conteudo_explicacao;
+
+import java.io.FileNotFoundException;
+
+public class TestaException {
+
+	public static void main(String[] args) throws FileNotFoundException {
+//		new java.io.FileInputStream("arquivo.txt");
+	}
+
+}
