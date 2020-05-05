318fc8a4198d10c901f16695a3e36a2b16f4afb2
Afonso Serafim Jacinto <aphonson1@gmail.com>
2020-05-05 15:40:16 -0300

    Update TestaContas.java

--- src/br/com/caelum/contas/main/TestaContas.java
+++ src/br/com/caelum/contas/main/TestaContas.java
@@ -1,10 +1,16 @@
 package br.com.caelum.contas.main;
 
+import br.com.caelum.contas.modelo.Conta;
+import br.com.caelum.contas.modelo.ContaCorrente;
 import br.com.caelum.javafx.api.main.SistemaBancario;
 
 public class TestaContas {
 
 	public static void main(String[] args) {
+		Conta cc = new ContaCorrente();
+		cc.setTitular("Afonso");
+		cc.setAgencia("1");
+		cc.setNumero(1);
 		SistemaBancario.mostraTela(true);
 		//TelaDeContas.main(args);
 	}
