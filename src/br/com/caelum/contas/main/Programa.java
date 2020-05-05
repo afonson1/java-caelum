14df5f4b389295f0b0aa04a568b144a646916895
Afonso Serafim Jacinto <aphonson1@gmail.com>
2020-05-05 15:20:20 -0300

    Update Programa.java

--- src/br/com/caelum/contas/main/Programa.java
+++ src/br/com/caelum/contas/main/Programa.java
@@ -1,8 +1,19 @@
 package br.com.caelum.contas.main;
 
+import br.com.caelum.contas.modelo.Conta;
+import br.com.caelum.contas.modelo.ContaCorrente;
+
 public class Programa {
 
 	public static void main(String[] args) {
+		Conta cc =  new ContaCorrente();
+		cc.deposita(100);
 		
+		try {
+			cc.saca(1000);
+		} catch (IllegalArgumentException e){
+			System.out.println(e.getMessage());
+		}
+		System.out.println("Conseguiu");
 	}
 }
