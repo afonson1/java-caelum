a9ca9470b2b076ce5b863114f6b34b8c9c556f13
Afonso Serafim Jacinto <aphonson1@gmail.com>
2020-05-14 15:27:31 -0300

    Create ArrayList_Aula15.java

---
+++ src/conteudo_explicacao/ArrayList_Aula15.java
@@ -0,0 +1,47 @@
+package conteudo_explicacao;
+
+import java.io.ObjectInputStream.GetField;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import br.com.caelum.contas.modelo.ContaCorrente;
+import br.com.caelum.javafx.api.controllers.ContasController;
+
+public class ArrayList_Aula15 {
+
+	public static void main(String[] args) {
+		List lista = new ArrayList();
+		lista.add("Manoel");
+		lista.add("Joaquim");
+		lista.add("Maria");
+		
+		ContaCorrente conta = new ContaCorrente();
+		conta.deposita(100);
+		
+		List listaDeContas = new ArrayList();
+		listaDeContas.add(conta);
+		
+		System.out.println(lista.size());
+		System.out.println(listaDeContas.size());
+		
+		for(int i = 0; i < lista.size(); i++){
+			System.out.println(lista.get(i));
+		}
+		
+		for(int i = 0; i < listaDeContas.size(); i++){
+			ContaCorrente contaAuxiliar = (ContaCorrente) listaDeContas.get(i);
+			System.out.println(contaAuxiliar.getSaldo());
+		}
+		
+		ArrayList<String> listaComGeneric = new ArrayList<String>();
+		listaComGeneric.add("1");
+		
+		System.out.println(lista);
+		
+		Collections.sort(lista);
+		
+		System.out.println(lista);		
+	}
+
+}
